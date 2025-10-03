adba7764fd1426f30a7a768b968cc01b67b0ca3f
ticketguy <samuelokwu85@gmail.com>
2025-10-03 23:44:41 +0100

    Final fix for PWA setup

--- sw.js
+++ sw.js
@@ -7,7 +7,11 @@ const FILES_TO_CACHE = [
   "index.html",
   "style.css",
   "app.js",
-  "icon-512.png",
+  "/favicon/site.webmanifest",
+  "/favicon/apple-touch-icon.png",
+  "/favicon/favicon-32x32.png",
+  "/favicon/favicon-16x16.png",
+  "/favicon/android-chrome-512x512.png", 
 ];
 
 // When the service worker is installed, open the cache and add the core files
